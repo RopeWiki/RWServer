f4ba217892c48e63f517d615209c0da51ce56566
Michelle Nilles <45955186+catware2@users.noreply.github.com>
2021-03-27 19:24:09 -0700

    Add Wikiloc encoded polyline decryption

--- rw/rw/Wikiloc.cpp
+++ rw/rw/Wikiloc.cpp
@@ -3,18 +3,15 @@
 
 #include "Wikiloc.h"
 #include "BETAExtract.h"
-#include "trademaster.h"
 #include "passwords.h"
-
 
 // ===============================================================================================
 
-#define WIKILOCTICKS 3000
-static double wikilocticks;
-
 int WIKILOC_ExtractKML(const char *ubase, const char *url, inetdata *out, int fx)
-{
+{	
 	DownloadFile f;
+
+	static double wikilocticks;
 	Throttle(wikilocticks, WIKILOCTICKS);
 	if (DownloadRetry(f, url))
 	{
@@ -75,18 +72,26 @@ void WIKILOC_ProcessTrack(vars* entry, vara* tracks, vara* waypoints, CString* c
 
 	//end waypoint
 	type = "e"; name = "End";
-	WIKILOC_ExtractWaypointCoords(entry, &type, "end",&name, waypoints, credit, url);
+	const auto end = WIKILOC_ExtractWaypointCoords(entry, &type, "end", &name, waypoints, credit, url);
 
 	name = ExtractString(*entry, "\"nom\":", "\"", "\"");
 
-	vars encodedPolyline = "geom:" + ExtractString(*entry, "\"geom\":", "\"", "\"");
+	const auto encodedPolyline = ExtractString(*entry, "\"geom\":", "\"", "\"");
 
-	//the following flow to decode the polyline string were converted from the embedded javascript on wikiloc:
-	//TODO: convert the javascript to decode the line to c++. For now, just pass it off to our javascript
+	//the following code to decode the polyline string was converted from the embedded javascript on wikiloc:
 	vara linelist;
-	linelist.push(encodedPolyline);
+	//linelist.push("geom:" + encodedPolyline.c_str()); //<- this was to pass it off to our javascript, before the decoding code was converted to C++
 	
-	tracks->push(KMLLine(name, *credit, linelist, OTHER, 3));
+	auto coords = WikilocCPPDecode::decodeWikilocEncodedPath(encodedPolyline);
+
+	for (int i = 0; i < coords.size(); i++) {
+		linelist.push(CCoord3(coords[i][1], coords[i][0]));
+	}
+	//the encoded polyline stops short of including the final 'end' waypoint. Add this to the end of the track
+	linelist.push(end);
+
+
+	tracks->push(KMLLine("Track", *credit, linelist, OTHER, 3));
 
 	//luca's original code to add coordinates:
 		
@@ -102,19 +107,21 @@ void WIKILOC_ProcessTrack(vars* entry, vara* tracks, vara* waypoints, CString* c
 	tracks->push(KMLLine("Track", name, linelist, OTHER, 3));*/
 }
 
-void WIKILOC_ExtractWaypointCoords(vars* entry, CString* type, const char* style, vars* name, vara* waypoints, CString* credit, const char *url)
+CString WIKILOC_ExtractWaypointCoords(vars* entry, CString* type, const char* style, vars* name, vara* waypoints, CString* credit, const char *url)
 {
 	const vars latStr = ExtractString(*entry, "\"" + *type + "lat\"", ":", ";");
 	const vars lngStr = ExtractString(*entry, "\"" + *type + "lng\"", ":", ";");
 
-	if (latStr.IsEmpty() || lngStr.IsEmpty()) return;
+	if (latStr.IsEmpty() || lngStr.IsEmpty()) return nullptr;
 
 	const double lat = CDATA::GetNum(latStr);
 	const double lng = CDATA::GetNum(lngStr);
 
-	if (!CheckLL(lat, lng, url)) return;
+	if (!CheckLL(lat, lng, url)) return nullptr;
 
 	waypoints->push(KMLMarker(style, lat, lng, *name, *credit));
+
+	return CCoord3(lat, lng);
 }
 
 int WIKILOC_DownloadPage(DownloadFile &f, const char *url, CSym &sym)
@@ -151,10 +158,10 @@ int WIKILOC_DownloadPage(DownloadFile &f, const char *url, CSym &sym)
 	vara wpt(f.memory, "maps.LatLng(");
 	vars description = stripHTML(ExtractString(f.memory, "class=\"description\"", ">", "</div"));
 	description.Replace(sym.GetStr(ITEM_DESC), "");
-	int n = wpt.length() - 2;
-	int len = description.length();
-	int score = n * 100 + len;
-	vars desc = MkString("%05d:%db+%dwpt", score, len, n);
+	const int n = wpt.length() - 2;
+	const int len = description.length();
+	const int score = n * 100 + len;
+	const vars desc = MkString("%05d:%db+%dwpt", score, len, n);
 	if (score < 300) sym.SetStr(ITEM_CLASS, "0:" + desc);
 	else sym.SetStr(ITEM_CLASS, "1:" + desc);
 
@@ -236,7 +243,8 @@ int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int (*DownloadPage)(
 
 int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int(*DownloadPage)(DownloadFile &f, const char *url, CSym &sym))
 {
-	int n = 0, step = 20;
+	int n = 0;
+	const int step = 20;
 	DownloadFile f;
 	if (f.Download("http://www.wikiloc.com/wikiloc/start.do"))
 	{
@@ -256,7 +264,7 @@ int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int(*DownloadPage)(D
 	while (true)
 	{
 		//vars url = "https://www.wikiloc.com/wikiloc/find.do?act=46%2C&uom=mi&s=id"+MkString("&from=%d&to=%d", from, from+step); 
-		vars url = "https://www.wikiloc.com/wikiloc/find.do?a=46&z=2&sw=-90%2C-180&ne=90%2C180" + MkString("&from=%d&to=%d", from, from + step);
+		const vars url = "https://www.wikiloc.com/wikiloc/find.do?a=46&z=2&sw=-90%2C-180&ne=90%2C180" + MkString("&from=%d&to=%d", from, from + step);
 		//vars url = https://www.wikiloc.com/wikiloc/find.do?a=46&z=2&from=0&to=15&sw=-90%2C-180&ne=90%2C180
 		if (f.Download(url)) // sw=-90%%2C-180&ne=90%%2C180&tr=0 https://www.wikiloc.com/wikiloc/find.do?act=46%2C&uom=mi&from=10&to=20
 		{
@@ -265,7 +273,7 @@ int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int(*DownloadPage)(D
 		}
 
 		//double count = ExtractNum(f.memory, "&nbsp;of&nbsp;", "", "</a");
-		double count = ExtractNum(f.memory, "count", ":", ",");
+		const double count = ExtractNum(f.memory, "count", ":", ",");
 		if (count <= 0 || count > 10000)
 		{
 			Log(LOGERR, "Count too big %s>10K", CData(count));
@@ -282,8 +290,8 @@ int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int(*DownloadPage)(D
 
 			vars name = stripHTML(ExtractString(list[i], "\"name\"", "\"", "\""));
 			vars loc = stripHTML(ExtractString(list[i], "\"near\"", "\"", "\""));
-			double lat = ExtractNum(list[i], "\"lat\"", ":", ",");
-			double lng = ExtractNum(list[i], "\"lon\"", ":", ",");
+			const double lat = ExtractNum(list[i], "\"lat\"", ":", ",");
+			const double lng = ExtractNum(list[i], "\"lon\"", ":", ",");
 			if (!CheckLL(lat, lng))
 			{
 				Log(LOGERR, "Invalid WIKILOC coords for %s", list[i]);
@@ -301,16 +309,16 @@ int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int(*DownloadPage)(D
 			sym.SetStr(ITEM_REGION, region);
 			sym.SetStr(ITEM_KML, "X");
 			vara rated(name, "*");
-			double stars = rated[0][strlen(rated[0]) - 1] - (double)'0';
+			const double stars = rated[0][strlen(rated[0]) - 1] - static_cast<double>('0');
 			if (stars >= 1 && stars <= 5)
 				sym.SetStr(ITEM_STARS, starstr(stars, 1));
 
-			printf("%d %d/%d %d/%d/%d    \r", symlist.GetSize(), i, list.length(), from, n, (int)count);
+			printf("%d %d/%d %d/%d/%d    \r", symlist.GetSize(), i, list.length(), from, n, static_cast<int>(count));
 			if (!UpdateCheck(symlist, sym) && MODE > -2)
 				continue;
 
 			if (DownloadPage(f, sym.id, sym))
-				Update(symlist, sym, NULL, FALSE);
+				Update(symlist, sym, nullptr, FALSE);
 		}
 
 		// next
@@ -335,3 +343,215 @@ int WIKILOC_DownloadBeta(const char *ubase, CSymList &symlist)
 	return TRUE;
 }
 
+//class to decode Wikiloc encoded polyline
+
+const std::string WikilocCPPDecode::base64_chars =
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"abcdefghijklmnopqrstuvwxyz"
+	"0123456789+/";
+
+std::vector<std::vector<double>> WikilocCPPDecode::decodeWikilocEncodedPath(const CString encodedPath)
+{	
+	auto byteArray = base64_decode(encodedPath);
+
+	auto coords = toGeoJSON(byteArray);
+
+	return coords;
+}
+
+std::vector<BYTE> WikilocCPPDecode::base64_decode(const CString encodedPath) {
+	int in_len = encodedPath.GetLength();
+	int i = 0;
+	int j;
+	int in_ = 0;
+	BYTE char_array_4[4], char_array_3[3];
+	std::vector<BYTE> ret;
+
+	while (in_len-- && (encodedPath[in_] != '=') && is_base64(encodedPath[in_])) {
+		char_array_4[i++] = encodedPath[in_]; in_++;
+		if (i == 4) {
+			for (i = 0; i < 4; i++)
+				char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+			for (i = 0; (i < 3); i++)
+				ret.push_back(char_array_3[i]);
+			i = 0;
+		}
+	}
+
+	if (i) {
+		for (j = i; j < 4; j++)
+			char_array_4[j] = 0;
+
+		for (j = 0; j < 4; j++)
+			char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+		char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+		char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+		char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+		for (j = 0; (j < i - 1); j++) ret.push_back(char_array_3[j]);
+	}
+
+	return ret;
+}
+
+std::vector<std::vector<double>> WikilocCPPDecode::toGeoJSON(std::vector<unsigned char> &buffer)
+{
+	auto wlData = new wikilocData();
+	wlData->buffer = buffer;
+	wlData->cursor = 0;
+	wlData->refpoint = std::vector<int>(4);
+
+	auto res = readBuffer(wlData);
+
+	std::vector<std::vector<double>> result = toCoords(res, wlData->ndims);
+
+	delete wlData;
+
+	return result;
+}
+
+std::vector<double> WikilocCPPDecode::readBuffer(wikilocData *wlData)
+{
+	int flag = wlData->buffer[wlData->cursor];
+
+	wlData->cursor++;
+
+	const auto precisionXy = unzigzag((240 & flag) >> 4);
+
+	wlData->factors = std::vector<double>(4);
+	wlData->factors[1] = std::pow(10, precisionXy);
+	wlData->factors[0] = wlData->factors[1];
+	flag = wlData->buffer[wlData->cursor];
+	wlData->cursor++;
+
+	int hasZ = 0, hasM = 0;
+
+	const auto extendedDims = (8 & flag) >> 3;
+
+	if (extendedDims > 0)
+	{
+		const auto extendedDimsFlag = wlData->buffer[wlData->cursor];
+		wlData->cursor++;
+		hasZ = 1 & extendedDimsFlag;
+		hasM = (2 & extendedDimsFlag) >> 1;
+		const auto precisionZ = (28 & extendedDimsFlag) >> 2;
+		const auto precisionM = (224 & extendedDimsFlag) >> 5;
+		if (hasZ > 0)
+		{
+			wlData->factors[2] = std::pow(10, precisionZ);
+		}
+		if (hasM > 0)
+		{
+			wlData->factors[2 + hasZ] = std::pow(10, precisionM);
+		}
+	}
+
+	const auto ndims = 2 + hasZ + hasM;
+	wlData->ndims = ndims;
+
+	const auto has_size = (2 & flag) >> 1;
+	if (has_size > 0)
+	{
+		ReadVarInt64(wlData); //originally had 'size =', need this here to advance the cursor
+	}
+
+	return readObjects(wlData);
+}
+
+std::vector<double> WikilocCPPDecode::readObjects(wikilocData *wlData)
+{
+	for (int i = 0; i < wlData->ndims; i++)
+	{
+		wlData->refpoint[i] = 0;
+	}
+
+	return parse_line(wlData);
+}
+
+std::vector<double> WikilocCPPDecode::parse_line(wikilocData *wlData)
+{
+	const auto npoints = ReadVarInt64(wlData);
+
+	return read_pa(wlData, npoints);
+}
+
+std::vector<double> WikilocCPPDecode::read_pa(wikilocData *wlData, int npoints)
+{
+	const auto ndims = wlData->ndims;
+	auto factors = wlData->factors;
+	auto coords = std::vector<double>(npoints * ndims);
+
+	for (int i = 0; i < npoints; i++)
+	{
+		for (int j = 0; j < ndims; j++)
+		{
+			wlData->refpoint[j] += ReadVarSInt64(wlData);
+			coords[ndims * i + j] = wlData->refpoint[j] / factors[j];
+		}
+	}
+
+	return coords;
+}
+
+int WikilocCPPDecode::ReadVarSInt64(wikilocData *wlData)
+{
+	const auto nVal = ReadVarInt64(wlData);
+	return unzigzag(nVal);
+}
+
+int WikilocCPPDecode::ReadVarInt64(wikilocData *wlData)
+{
+	auto cursor = wlData->cursor;
+	auto nVal = 0;
+	auto nShift = 0;
+
+	while (true)
+	{
+		const int nByte = wlData->buffer[cursor];
+
+		if ((128 & nByte) == 0)
+		{
+			cursor++;
+			wlData->cursor = cursor;
+			return nVal | nByte << nShift;
+		}
+
+		nVal |= (127 & nByte) << nShift;
+		cursor++;
+		nShift += 7;
+	}
+}
+
+int WikilocCPPDecode::unzigzag(int nVal)
+{
+	return (1 & nVal) == 0 ? nVal >> 1 : -(nVal >> 1) - 1;
+}
+
+std::vector<std::vector<double>> WikilocCPPDecode::toCoords(std::vector<double> &coordinates, int ndims)
+{
+	auto coords = std::vector<std::vector<double>>(coordinates.size() / ndims);
+
+	auto index = 0;
+
+	const int size = coordinates.size();
+
+	for (int i = 0; i < size; i += ndims)
+	{
+		auto pos = std::vector<double>(4);
+
+		for (int c = 0; c < ndims; ++c)
+		{
+			pos[c] = coordinates[i + c];
+		}
+
+		coords[index++] = pos;
+	}
+
+	return coords;
+}
--- rw/rw/Wikiloc.h
+++ rw/rw/Wikiloc.h
@@ -7,6 +7,8 @@
 
 #define WIKILOC "wikiloc.com"
 
+#define WIKILOCTICKS 3000
+
 #define MAXWIKILOCLINKS 5
 
 int WIKILOC_ExtractKML(const char *ubase, const char *url, inetdata *out, int fx);
@@ -15,4 +17,53 @@ int WIKI_DownloadBeta(const char *ubase, CSymList &symlist, int(*DownloadPage)(D
 int WIKILOC_DownloadBeta(const char *ubase, CSymList &symlist);
 void WIKILOC_ProcessWaypoint(vars* entry, vara* waypoints, CString* credit, const char* url);
 void WIKILOC_ProcessTrack(vars* entry, vara* tracks, vara* waypoints, CString* credit, const char *url);
-void WIKILOC_ExtractWaypointCoords(vars* entry, CString* type, const char* style, vars* name, vara* waypoints, CString* credit, const char *url);
+CString WIKILOC_ExtractWaypointCoords(vars* entry, CString* type, const char* style, vars* name, vara* waypoints, CString* credit, const char *url);
+
+
+//class to decode Wikiloc encoded polyline
+#include <string>
+#include <vector>
+
+typedef unsigned char BYTE;
+
+class WikilocCPPDecode
+{
+public:
+	class wikilocData
+	{
+	public:
+		std::vector<unsigned char> buffer;
+		int cursor = 0;
+		std::vector<int> refpoint;
+		int ndims = 0;
+		std::vector<double> factors;
+	};
+
+	static std::vector<std::vector<double>> decodeWikilocEncodedPath(CString encodedPath);
+
+	static std::vector<BYTE> base64_decode(CString encoded_string);
+
+	static std::vector<std::vector<double>> toGeoJSON(std::vector<unsigned char> &buffer);
+
+	static std::vector<double> readBuffer(wikilocData *wlData);
+
+	static std::vector<double> readObjects(wikilocData *wlData);
+
+	static std::vector<double> parse_line(wikilocData *wlData);
+
+	static std::vector<double> read_pa(wikilocData *wlData, int npoints);
+
+	static int ReadVarSInt64(wikilocData *wlData);
+
+	static int ReadVarInt64(wikilocData *wlData);
+
+	static int unzigzag(int nVal);
+
+	static std::vector<std::vector<double>> toCoords(std::vector<double> &coordinates, int ndims);
+
+	static bool is_base64(const unsigned char c) {
+		return isalnum(c) || c == '+' || c == '/';
+	}
+
+	static const std::string base64_chars;
+};
